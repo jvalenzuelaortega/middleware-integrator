ede5ec0ed616da3f92edafc20bd1b695fa70a5d3
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2024-10-22 19:36:46 -0300

    FEATURE: add properties for k8s

--- middleware/src/main/java/com/example/middleware/infrastructure/api/AssistanceApiClient.java
+++ middleware/src/main/java/com/example/middleware/infrastructure/api/AssistanceApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.middleware.config.FeignConfig;
 import com.example.middleware.infrastructure.api.dto.CompanyDto;
 
-@FeignClient(value = "empresa", url = "http://localhost:5000", configuration = FeignConfig.class)
+@FeignClient(value = "empresa", url = "http://flask-service:5000", configuration = FeignConfig.class)
 public interface AssistanceApiClient {
 
 	@GetMapping("/get-assistance-by-company")
